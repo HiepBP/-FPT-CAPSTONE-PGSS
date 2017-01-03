da8516e93f7fbdd51bb21bdab9a54e992f286915
Hinaka <okami.shiroi212@gmail.com>
2017-01-03 16:02:15 +0700

    Initialize Timber logger

--- Android/app/src/main/java/com/fptuni/capstone/pgss/MyApplication.java
+++ Android/app/src/main/java/com/fptuni/capstone/pgss/MyApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 
 import com.facebook.stetho.Stetho;
 
+import timber.log.Timber;
+
 /**
  * Created by TrungTNM on 1/3/2017.
  */
@@ -14,5 +16,9 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
     }
 }
