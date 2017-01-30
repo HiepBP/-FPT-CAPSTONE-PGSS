49ac29cf9d057d0cea966fe6eee4bfffa769a7f2
Hinaka <okami.shiroi212@gmail.com>
2017-01-30 13:59:53 +0700

    Add HMC5883L module

---
+++ Arduino/HMC5883L.h
@@ -0,0 +1,24 @@
+/*
+	HMC5883L.h - An Arduino library for Triple Axis Magnetometer HMC5883L
+*/
+
+#ifndef HMC5883L_h
+#define HMC5883L_h
+
+#include "Arduino.h"
+
+class HMC5883L
+{
+	public:
+		HMC5883L(int16_t xOffset, int16_t zOffset, int16_t yOffset);
+		void setup();
+		boolean isInRange();
+	private:
+		void readData(int16_t* x, int16_t* z, int16_t* y);
+		void setupStabilityValue();
+		int16_t X_OFFSET;
+		int16_t Z_OFFSET;
+		int16_t Y_OFFSET;
+};
+
+#endif
