3a96d09cea3484451737e1ed5687912414c388a7
Hinaka <okami.shiroi212@gmail.com>
2017-02-22 22:09:48 +0700

    Add logic for Login and Register api

--- Android/app/src/main/java/com/fptuni/capstone/pgss/activities/LoginActivity.java
+++ Android/app/src/main/java/com/fptuni/capstone/pgss/activities/LoginActivity.java
@@ -12,10 +12,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fptuni.capstone.pgss.R;
+import com.fptuni.capstone.pgss.interfaces.AccountClient;
+import com.fptuni.capstone.pgss.models.Account;
+import com.fptuni.capstone.pgss.network.AccountPackage;
+import com.fptuni.capstone.pgss.network.ServiceGenerator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A login screen that offers login via email/password.
@@ -40,8 +47,39 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_login)
     void onLoginButtonClick(View view) {
-        Toast.makeText(this, "Login button click", Toast.LENGTH_SHORT).show();
-        // TODO: login process
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        Account account = new Account(username, password);
+        AccountClient client = ServiceGenerator.createService(AccountClient.class);
+        Call<AccountPackage> call = client.login(account);
+        call.enqueue(new Callback<AccountPackage>() {
+            @Override
+            public void onResponse(Call<AccountPackage> call, Response<AccountPackage> response) {
+                final AccountPackage result = response.body();
+                if (result.isSuccess()) {
+                    //TODO: store account information
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), result.getMessage(), Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AccountPackage> call, final Throwable t) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+            }
+        });
     }
 
     @OnClick(R.id.textview_login_register)
--- Android/app/src/main/java/com/fptuni/capstone/pgss/activities/RegisterActivity.java
+++ Android/app/src/main/java/com/fptuni/capstone/pgss/activities/RegisterActivity.java
@@ -9,10 +9,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fptuni.capstone.pgss.R;
+import com.fptuni.capstone.pgss.interfaces.AccountClient;
+import com.fptuni.capstone.pgss.models.Account;
+import com.fptuni.capstone.pgss.network.AccountPackage;
+import com.fptuni.capstone.pgss.network.ServiceGenerator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -40,13 +47,69 @@ public class RegisterActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_register)
     void onRegisterButtonClick(View view) {
-        Toast.makeText(this, "On register click", Toast.LENGTH_SHORT).show();
-        // TODO: register click
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String confirmPassword = etConfirmPassword.getText().toString();
+        String username = etUsername.getText().toString();
+        String fullname = etFullname.getText().toString();
+
+        if (!validateEmail(email)) {
+            return;
+        }
+        if (!validatePassword(password, confirmPassword)) {
+            return;
+        }
+
+        Account account = new Account(username, password);
+        account.setConfirmPassword(confirmPassword);
+        account.setEmail(email);
+        account.setFullname(fullname);
+        AccountClient client = ServiceGenerator.createService(AccountClient.class);
+        Call<AccountPackage> call = client.register(account);
+        call.enqueue(new Callback<AccountPackage>() {
+            @Override
+            public void onResponse(Call<AccountPackage> call, Response<AccountPackage> response) {
+                final AccountPackage result = response.body();
+                if (result.isSuccess()) {
+                    // finish create, back to Login Activity
+                    finish();
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), result.getMessage(), Toast.LENGTH_SHORT)
+                                    .show();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<AccountPackage> call, final Throwable t) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+            }
+        });
+    }
+
+    private boolean validateEmail(String email) {
+        // TODO: validate email format
+        return true;
+    }
+
+    private boolean validatePassword(String password, String confirmPassword) {
+        // TODO: validate password
+        return true;
     }
 
     @OnClick(R.id.textview_register_login)
     void onLoginTextClick(View view) {
-        // return to login activiy
+        // return to login activity
         finish();
     }
 }
